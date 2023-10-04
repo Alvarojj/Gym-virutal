d488a7847a6c854cc68c0f0c3fc67244eb02411c
Alvarojj <aejimenez19@gmail.com>
2023-10-04 00:09:55 -0500

    Creacion de menu principal y agregacion de boostrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InicioComponent} from './componentes/principal/inicio/inicio.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'Inicio',
+    component: InicioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
