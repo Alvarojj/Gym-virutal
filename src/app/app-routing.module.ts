a0c27be6ac6e5374fef32a74d39efc3d2cb64060
eduarsolaez <eduarsoli703@gmail.com>
2023-10-05 19:33:03 -0500

    Subiendo Vista Seleccionar Entrenador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {VistaSeleccionarEntrenadorComponent} from './Componentes/vista-seleccionar-entrenador/vista-seleccionar-entrenador.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'VistaSeleccionarEntrenador',
+    component: VistaSeleccionarEntrenadorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
