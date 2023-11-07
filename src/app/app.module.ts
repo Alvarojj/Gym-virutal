a6a3cbfb5d11c86a8d3f50147b88d99bd2e42697
campo22 <miguelcampodiaz@gmail.com>
2023-11-07 18:51:11 -0500

    primer envio de inicio entrenador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { RutinaComponent } from './componentes/principal/rutina/rutina.component
 import { SeleccionaPlanComponent } from './componentes/selecciona-plan/selecciona-plan.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { CrearCuentaComponent } from './componentes/crear-cuenta/crear-cuenta.component';
+import { InicioEntrenadoresComponent } from './componentes/entrenadores/inicio-entrenadores/inicio-entrenadores.component';
+import { LoginEntrenadoresComponent } from './componestes/entrenadores/login-entrenadores/login-entrenadores.component';
+import { Menu2Component } from './componentes/entrenadores/menu2/menu2.component';
 
 
 @NgModule({
@@ -23,7 +26,10 @@ import { CrearCuentaComponent } from './componentes/crear-cuenta/crear-cuenta.co
     RutinaComponent,
     SeleccionaPlanComponent,
     LoginComponent,
-    CrearCuentaComponent
+    CrearCuentaComponent,
+    InicioEntrenadoresComponent,
+    LoginEntrenadoresComponent,
+    Menu2Component
   ],
   imports: [
     BrowserModule,
