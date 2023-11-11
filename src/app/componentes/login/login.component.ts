cd9ee83aaf6c545be30b77a12792f44bb2320d38
Alvarojj <aejimenez19@gmail.com>
2023-11-11 17:21:53 -0500

    subiendo inicio de seccion funcional

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClientesService } from 'src/app/services/clientes.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  constructor(private clienteService: ClientesService, private router: Router ){
 
+  }
+
+  ngOnInit(): void{  }
+
+  correo?: string;
+  clave?: string;
+
+  iniarSeccion(){
+    let cliente = null
+    let clien =     {
+      "idPersona": {
+          "correo": this.correo,
+          "clave": this.clave
+      }
+    }
+    this.clienteService.getClientes(clien)
+    .subscribe(resp => {
+      cliente = resp
+      if(cliente == null){
+        alert('Usuario o clave incorrecto')
+      }else if(cliente != null){
+          this.router.navigate(['/Inicio']) 
+        }
+    })
+  }
 }
